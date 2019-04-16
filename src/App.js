3d00b6bfdf774b4c6d5f9ca90b3ffe3b9d6ad9e0
AB <aarushban@gmail.com>
2019-04-15 21:57:36 -0400

    parts of list working

--- src/App.js
+++ src/App.js
@@ -5,14 +5,34 @@ import {StyleSheet, css} from 'aphrodite';
 import styled, { keyframes } from 'styled-components';
 import { inherits } from 'util';
 
-var map3 = new Map([["nature", [ "farms", "parks", "nature trails", "exercising", "beaches", "hikes", "biking", "running", "outdoor games (i.e. hide and seek, tag, etc.)", "outdoor sports (i.e. football, basketball, etc.)", "road trips"]], 
+var list = new Map([["nature", [ "farms", "parks", "nature trails", "exercising", "beaches", "hikes", "biking", "running", "outdoor games (i.e. hide and seek, tag, etc.)", "outdoor sports (i.e. football, basketball, etc.)", "road trips"]], 
             ["city" , ["shopping", "clubs", "bars", "lounges", "restaurants", "museums", "concerts"]], 
             ["indoors", ["movies/TV shows", "board games", "watching random videos", "forums", "sleeping", "reading", "cooking", "personal projects"]]]); 
 
+console.log(list.get("nature"));
+window.onload = function () {
+  var select = document.getElementById("first");
+  console.log("select is " + select);
+  var options = list.get("nature");
+  console.log(options + " and the length is " + options.length);
+  
+  for(var i = 0; i < options.length; i++) {
+      var opt = options[i];
+      console.log("options at i is " + options[i]);
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      console.log("el is " + el.value);
+      select.appendChild(el);
+  }
+};
 
+
+  // console.log("jdksaljdlas");
 class App extends Component {
   render() {
     return (
+      // console.log("djsakdjslakdjlaksjl"),
     <body className = {css(styles.body)}>
       <p className = {css(styles.FadeAnimations)}> Welcome!</p>
       <div className = {css(styles.containBlocks)}>
@@ -24,68 +44,52 @@ class App extends Component {
       <div className = {css(styles.blocks)}>
              <form action="/Chat.js">
              <p className = {css(styles.label)}> What do you like to do in the weekend?</p>
-               <label for="fname" className = {css(styles.label)}>Choose a username!</label>
+               <label htmlFor="fname" className = {css(styles.label)}>Choose a username!</label>
                <input type="text" id="uNameNew" name="username" placeholder="Username" className = {css(styles.input)}></input>
 
-               <label for="password" className = {css(styles.label)}>Create a password!</label>
+               <label htmlFor="password" className = {css(styles.label)}>Create a password!</label>
                <input type="password" className = {css(styles.input)} id="password" name="password" placeholder="password"></input>
    
 
-               <label for="first" className = {css(styles.label)}>First</label>
-                  <select id="first" name="first" className = {css(styles.input)}>
-                       <option value="australia">Australia</option>
-                              <option value="canada">Canada</option>
-                              <option value="usa">USA</option>
+               <label htmlFor="first" className = {css(styles.label)}>First</label> 
+                  <select id="first" className = {css(styles.input)}>
+                       <option value="default">parks</option>
                   </select>
+               
 
-              <label for="second" className = {css(styles.label)}>Second</label>
+              {/* <label for="second" className = {css(styles.label)}>Second</label>
                   <select id="second" name="second" className = {css(styles.input)}>
                        <option value="australia">Australia</option>
                               <option value="canada">Canada</option>
                               <option value="usa">USA</option>
-                  </select>
-              <label for="third" className = {css(styles.label)}>Third</label>
-                  <select id="third" name="third" className = {css(styles.input)}>
-                       <option value="australia">Australia</option>
-                              <option value="canada">Canada</option>
-                              <option value="usa">USA</option>
-                  </select>
-
-              <label for="fourth" className = {css(styles.label)}>Fourth</label>
-                  <select id="fourth" name="fourth" className = {css(styles.input)}>
-                       <option value="australia">Australia</option>
-                              <option value="canada">Canada</option>
-                              <option value="usa">USA</option>
-                  </select>
-
-              <label for="fifth" className = {css(styles.label)}>Fifth</label>
-                  <select id="fifth" name="fifth" className = {css(styles.input)} >
-                       <option value="australia">Australia</option>
-                              <option value="canada">Canada</option>
-                              <option value="usa">USA</option>
-                  </select>
-               {/* <label for="fifth" className = {css(styles.label)}>Fifth</label>
-               <input type="text" className = {css(styles.input)} id="fifth" name="fifth" placeholder="Your fifth item!"></input> */}
+                </select>*/}
              
                <button type="submit" value="Submit"></button>
            </form>
       </div>
       <div className = {css(styles.blocks)}>
              <form action="/Chat.js">
-               <label for="name" className = {css(styles.label)}>Username</label>
+               <label htmlFor="name" className = {css(styles.label)}>Username</label>
                <input type="text" id="uNameLogin" name="username" placeholder="username" className = {css(styles.input)}></input>
            
-               <label for="password" className = {css(styles.label)}>Password</label>
+               <label htmlFor="password" className = {css(styles.label)}>Password</label>
                <input type="password" className = {css(styles.input)} id="password" name="password" placeholder="password"></input>
                <button type="submit" value="Submit"></button>
            </form>
         </div>
-      </div>
-   </body>
-   
+      </div>  
+   </body> 
     );
+
+    
+
+      
   }
+
+  
 }
+
+
 
 
 const styles = StyleSheet.create({
