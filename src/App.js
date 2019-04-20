c7804c109365c7376ca8a545d70230315a7e07c1
lukecheng1998 <lukecheng1998@gmail.com>
2019-04-20 01:18:12 -0400

    routing works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {StyleSheet, css} from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite';
 import styled, { keyframes } from 'styled-components';
 import { inherits } from 'util';
 import { withRouter } from 'react-router-dom';
-import { BrowserRouter as Router, route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import firebase from './firebase'; // use this when database is implemented
 import Chat from "./Chat";
 import login from "./App";//TODO: You will need to remove this and have it reference the game that you guys are making
@@ -37,140 +37,111 @@ lukepassword.on('value', (p) => {
 //We will have to get this from the data base itself, and set vars accordingly and then compare with the input some how
 
 //check if passwords match
-aut.signInWithEmailAndPassword(setUsername, password).catch(function(error){
+aut.signInWithEmailAndPassword(setUsername, password).catch(function (error) {
   var errorCode = error.code;
   var errorMessage = error.message;
   console.log(errorCode);
   console.log(errorMessage);
 })
 /*SIGN OUT*/
-aut.signOut().then(function(){
+aut.signOut().then(function () {
   //Signout successful
   console.log("Signed out!");
 
-}).catch(function(error){
+}).catch(function (error) {
   console.log(error);
 })
 /*CHECK IF USER IS SIGNED IN */
-aut.onAuthStateChanged(function(user){
-  if(user){
+aut.onAuthStateChanged(function (user) {
+  if (user) {
     //Return true if user is signed in
     return true;
-  }else{
+  } else {
     //Return false
     return false;
   }
 })
 //*/ //End of the comments
 
-var list = new Map([["nature", [ "farms", "parks", "nature trails", "exercising", "beaches", "hikes", "biking", "running", "outdoor games (i.e. hide and seek, tag, etc.)", "outdoor sports (i.e. football, basketball, etc.)", "road trips"]], 
-            ["city" , ["shopping", "clubs", "bars", "lounges", "restaurants", "museums", "concerts"]], 
-            ["indoors", ["movies/TV shows", "board games", "watching random videos", "forums", "sleeping", "reading", "cooking", "personal projects", "Conversation Games (i.e. Never Have I ever, Truth or Dare)"]]]); 
-
+var list = new Map([["nature", ["farms", "parks", "nature trails", "exercising", "beaches", "hikes", "biking", "running", "outdoor games (i.e. hide and seek, tag, etc.)", "outdoor sports (i.e. football, basketball, etc.)", "road trips"]],
+["city", ["shopping", "clubs", "bars", "lounges", "restaurants", "museums", "concerts"]],
+["indoors", ["movies/TV shows", "board games", "watching random videos", "forums", "sleeping", "reading", "cooking", "personal projects", "Conversation Games (i.e. Never Have I ever, Truth or Dare)"]]]);
 var IDs = ["first", "second", "third", "fourth", "fifth"]; //five options to fill
-// console.log(list.get("nature"));
-window.onload = function () {
+var options = (list.get("nature").concat(list.get("city"))).concat(list.get("indoors"))
 
-for(var j  = 0; j < IDs.length; j++){ //outer loop for 5 options
-  console.log("THE J IS " + IDs[j]);
-  var select = document.getElementById(IDs[j]);
-  // console.log("select is " + select);
-  var options = (list.get("nature").concat(list.get("city"))).concat(list.get("indoors"));
-  console.log(options + " and the length is " + options.length);
-  for(var i = 0; i < options.length; i++) {
-      var opt = options[i];
-      // console.log("options at i is " + options[i]);
-      var element = document.createElement("option");
-      element.textContent = opt;
-      element.value = opt;
-      // console.log("el is " + el.value);
-      select.appendChild(element);
-  }
-}
-};
-
-
-  // console.log("jdksaljdlas");
 class App extends Component {
+
+  renderOptions() {
+    return options.map((o) => (
+      <option key={o} value={o}>{o}</option>
+    ))
+  }
+
+  renderSelects() {
+    return IDs.map((id) => (
+      <div key={id}>
+        <label htmlFor={id} className={css(styles.label)}>{id}</label>
+        <select id={id} className={css(styles.input)}>
+          <option defaultValue disabled>Please select an option: </option>
+          {this.renderOptions()}
+        </select>
+      </div>
+    ))
+  }
+
   render() {
     return (
-      // console.log("djsakdjslakdjlaksjl"),
-    
-    <div className = {css(styles.body)}>
-      <p className = {css(styles.FadeAnimations)}> Welcome!</p>
-      <div className = {css(styles.containBlocks)}>
-          <p className = {css(styles.FadeAnimations2)}>New User? Start here!</p>
-          <p className = {css(styles.FadeAnimations2)}>Already have an account? Login here!</p>
-     </div>
-
-      <div className = {css(styles.containBlocks)}>
-      <div className = {css(styles.blocks)}>
-             <form action="/Chat.js">
-             <p className = {css(styles.label)}> What do you like to do in the weekend?</p>
-               <label htmlFor="fname" className = {css(styles.label)}>Choose a username!</label>
-               <input type="text" id="uNameNew" name="username" placeholder="Username" className = {css(styles.input)}></input>
-
-               <label htmlFor="password" className = {css(styles.label)}>Create a password!</label>
-               <input type="password" className = {css(styles.input)} id="password" name="password" placeholder="password"></input>
-   
-
-               <label htmlFor="first" className = {css(styles.label)}>First</label> 
-                  <select id="first" className = {css(styles.input)}>
-                  <option value="default"> </option>
-                  </select>
-
-              <label htmlFor="second" className = {css(styles.label)}>Second</label> 
-                  <select id="second" className = {css(styles.input)}>
-                  <option value="default"> </option>
-                  </select>
-
-              <label htmlFor="third" className = {css(styles.label)}>Third</label> 
-                  <select id="third" className = {css(styles.input)}>
-                  <option value="default"> </option>
-                  </select>
-
-              <label htmlFor="fourth" className = {css(styles.label)}>Fourth</label> 
-                  <select id="fourth" className = {css(styles.input)}>
-                  <option value="default"> </option>
-                  </select>
-
-              <label htmlFor="fifth" className = {css(styles.label)}>Fifth</label> 
-                  <select id="fifth" className = {css(styles.input)}>
-                  <option value="default"> </option>
-                  </select>
-                 <Router>
-                   <div>
-
-                   </div>
-                 </Router>
-               {/* <button type="submit" value="Submit"></button> */}
-           </form>
-      </div>
-      <div className = {css(styles.blocks)}>
-             <form action="/Chat.js">
-               <label htmlFor="name" className = {css(styles.label)}>Username</label>
-               <input type="text" id="uNameLogin" name="username" type="username" placeholder="username" className = {css(styles.input)}></input>
-           
-               <label htmlFor="password" className = {css(styles.label)}>Password</label>
-               <input type="password" className = {css(styles.input)} id="password" name="password" type="password" placeholder="password"></input>
-               <button type="submit" value="Submit">Login</button>
-           </form>
-           <Router>
-             <div>
-               <Router exact path= "/Chat" Component = {Chat}/>
-             </div>
-           </Router>
+      <div className={css(styles.body)}>
+        <p className={css(styles.FadeAnimations)}> Welcome!</p>
+        <div className={css(styles.containBlocks)}>
+          <p className={css(styles.FadeAnimations2)}>New User? Start here!</p>
+          <p className={css(styles.FadeAnimations2)}>Already have an account? Login here!</p>
         </div>
-      </div>  
-   </div> 
-    );
-  
-    
 
-      
+        <div className={css(styles.containBlocks)}>
+          <div className={css(styles.blocks)}>
+            <form action="/Chat.js">
+              <p className={css(styles.label)}> What do you like to do in the weekend?</p>
+              <label htmlFor="fname" className={css(styles.label)}>Choose a username!</label>
+              <input type="text" id="uNameNew" name="username" placeholder="Username" className={css(styles.input)}></input>
+
+              <label htmlFor="password" className={css(styles.label)}>Create a password!</label>
+              <input type="password" className={css(styles.input)} id="password" name="password" placeholder="password"></input>
+
+              {this.renderSelects()}
+              <Router>
+                <div>
+
+                </div>
+              </Router>
+              {/* <button type="submit" value="Submit"></button> */}
+            </form>
+          </div>
+          <div className={css(styles.blocks)}>
+            <form action="/Chat.js">
+              <label htmlFor="name" className={css(styles.label)}>Username</label>
+              <input type="text" id="uNameLogin" name="username" type="username" placeholder="username" className={css(styles.input)}></input>
+
+              <label htmlFor="password" className={css(styles.label)}>Password</label>
+              <input type="password" className={css(styles.input)} id="password" name="password" type="password" placeholder="password"></input>
+              <button type="submit" value="Submit">Login</button>
+            </form>
+            <Router>
+              <div>
+                <Router exact path="/Chat" component={Chat} />
+              </div>
+            </Router>
+          </div>
+        </div>
+      </div>
+    );
+
+
+
+
   }
 
-  
+
 }
 
 
@@ -181,89 +152,89 @@ const styles = StyleSheet.create({
   html: {
     backgroundColor: 'black',
     overflow: 'hidden',
-  }, 
+  },
 
   body: {
     backgroundColor: 'black',
     overflow: 'hidden',
-  }, 
-  
+  },
+
   FadeAnimations: {
     animationName: 'fadein',
     animationDuration: '3s',
     textAlign: 'center',
-    color: 'white', 
-    fontSize: '80px', 
-    width: '100%', 
+    color: 'white',
+    fontSize: '80px',
+    width: '100%',
     padding: '10px',
- },
+  },
 
- FadeAnimations2: {
-  animationName: 'fadein',
-  animationDuration: '5s',
-  textAlign: 'center',
-  color: '#95eff9', 
-  fontSize: '30px', 
-  paddingLeft: '20px', 
-  paddingRight: '20px', 
-  width: '40%', 
-},
+  FadeAnimations2: {
+    animationName: 'fadein',
+    animationDuration: '5s',
+    textAlign: 'center',
+    color: '#95eff9',
+    fontSize: '30px',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    width: '40%',
+  },
 
-blocks: {
-  animationName: 'fadein',
-  animationDuration: '3s',
-  display: 'inline-block',
-  textAlign: 'center',
-  color: 'white', 
-  fontSize: '30px', 
-  width: '40%',
-  height: '20%', 
-  overflow: 'auto', 
-  // float: 'left',
-  paddingLeft: '20px', 
-  paddingRight: '20px', 
-  borderRadius: '5px',
-  // height: '50vh',
-  backgroundColor: '#f2f2f2',
-},
+  blocks: {
+    animationName: 'fadein',
+    animationDuration: '3s',
+    display: 'inline-block',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: '30px',
+    width: '40%',
+    height: '20%',
+    overflow: 'auto',
+    // float: 'left',
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    borderRadius: '5px',
+    // height: '50vh',
+    backgroundColor: '#f2f2f2',
+  },
 
-containBlocks: {
-  display: 'flex',
-  paddingLeft: '30px',
-  paddingRight: '30px',
-  paddingBottom: '30px',
-  justifyContent:'space-between',
-  position: 'relative',
-  backgroundColor: 'black',
-},
+  containBlocks: {
+    display: 'flex',
+    paddingLeft: '30px',
+    paddingRight: '30px',
+    paddingBottom: '30px',
+    justifyContent: 'space-between',
+    position: 'relative',
+    backgroundColor: 'black',
+  },
 
- div: {
-  // width: '75%',
-  // height: '75%',
+  div: {
+    // width: '75%',
+    // height: '75%',
 
-  position: 'absolute',
-  top:'0',
-  bottom: '0',
-  left: '0',
-  right: '0',
-    
-  margin: 'auto',
-  // overflow: 'hidden',
- },
- 
- input : {
-  width: '100%',
-  padding: '20px',
-  margin: '8px 0',
-  display: 'inline-block',
-  border: '1px solid #ccc',
-  borderRadius: '4px',
-  boxSizing: 'border-box',
-  // overflow: 'hidden',
- 
- }, 
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    left: '0',
+    right: '0',
 
- button: {
+    margin: 'auto',
+    // overflow: 'hidden',
+  },
+
+  input: {
+    width: '100%',
+    padding: '20px',
+    margin: '8px 0',
+    display: 'inline-block',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    boxSizing: 'border-box',
+    // overflow: 'hidden',
+
+  },
+
+  button: {
     width: '100%',
     backgroundColor: '#4CAF50',
     color: 'white',
@@ -273,13 +244,13 @@ containBlocks: {
     borderRadius: '4px',
     cursor: 'pointer',
     // overflow: 'hidden',
- },
+  },
 
   label: {
-   color: 'black', 
-   fontSize: '30px',
-   padding: '20px',
- },
+    color: 'black',
+    fontSize: '30px',
+    padding: '20px',
+  },
 
 
 })
--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
-import { withRouter } from "react-router-dom"
 import app from "firebase";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Chat from './Chat';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
+
+
+//ReactDOM.render(<Chat />, document.getElementById('root'))
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-/* My Code Starts here*/
-class SignInContainer extends Component {
-    handleSignIn = async event => {
-        const { username, password } = event.target.elements;
-        try{
-            const user = await app
-            .auth()
-            .signInWithEmailAndPassword(username.value, password.value);
-            this.props.history.push("/")
-        } catch(error){
-            alert(error);
-        }
-    };
+function Wrapper(props) {
+    return (
+        <Router>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/Chat" component={Chat}/>
+        </Router>
+    )
 }
-/*My code ends here */
-//I can't use on submit
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Wrapper/>, document.getElementById('root'));
 serviceWorker.unregister();
