be6799cb3d2fe69b258a3c34f2a470f6d00e9e74
AB <aarushban@gmail.com>
2019-04-23 23:51:58 -0400

    Room added

---
+++ src/Room.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { StyleSheet, css } from 'aphrodite'
+
+const Room = ({ roomName }) => {
+  return (
+    <li className={css(styles.item)}>
+      <NavLink
+        to={`/chat/rooms/${roomName}`}
+        className={css(styles.link)}
+      >
+        {roomName}
+      </NavLink>
+    </li>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    marginBottom: '0.5rem',
+  },
+
+  link: {
+    display: 'block',
+    color: 'whitesmoke',
+    textDecoration: 'none',
+
+    '::before': {
+      content: '"# "',
+    },
+
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    },
+  },
+})
+
+export default Room
